31d2784eb5ec844a55a7ac2b06ea24d9974ce5df
Jan Urban Neto <jan.urbanneto@gmail.com>
2021-10-23 23:43:44 -0300

    login logic

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,39 +1,44 @@
+import { api } from '../../services/api'
 import styles from './styles.module.scss'
 import logoImg from '../../assets/logo.svg'
+import { useEffect, useState } from 'react'
+
+type Message = {
+   id: string;
+   text: string;
+   user: {
+      name: string;
+      avatar_url: string;
+   }
+}
 
 export function MessageList() {
-   return(
+   const [messages, setMessages] = useState<Message[]>([])
+
+   useEffect(() => {
+      api.get<Message[]>('messages/last3').then(response => {
+         setMessages(response.data)
+      })
+   }, [])
+
+   return (
       <div className={styles.messageListWrapper}>
          <img src={logoImg} alt="DoWhile 2021" />
 
          <ul className={styles.messageList}>
-            <li className={styles.message}>
-               <p className={styles.messageContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eaque maiores officiis error qui suscipit dicta quasi aut dolorum, tempora enim accusamus! Id doloremque sunt soluta facere assumenda error at.</p>
-               <div className={styles.messageUser}>
-                  <div className={styles.userImage}>
-                     <img src="https://github.com/JanUrbanNeto.png" alt="Dinirso Alafuca" />
-                  </div>
-                  <span>Dinirso Alafuca</span>
-               </div>
-            </li>
-            <li className={styles.message}>
-               <p className={styles.messageContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eaque maiores officiis error qui suscipit dicta quasi aut dolorum, tempora enim accusamus! Id doloremque sunt soluta facere assumenda error at.</p>
-               <div className={styles.messageUser}>
-                  <div className={styles.userImage}>
-                     <img src="https://github.com/JanUrbanNeto.png" alt="Dinirso Alafuca" />
-                  </div>
-                  <span>Dinirso Alafuca</span>
-               </div>
-            </li>
-            <li className={styles.message}>
-               <p className={styles.messageContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eaque maiores officiis error qui suscipit dicta quasi aut dolorum, tempora enim accusamus! Id doloremque sunt soluta facere assumenda error at.</p>
-               <div className={styles.messageUser}>
-                  <div className={styles.userImage}>
-                     <img src="https://github.com/JanUrbanNeto.png" alt="Dinirso Alafuca" />
-                  </div>
-                  <span>Dinirso Alafuca</span>
-               </div>
-            </li>
+            {messages.map(message => {
+               return (
+                  <li key={message.id} className={styles.message}>
+                     <p className={styles.messageContent}>{message.text}</p>
+                     <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                           <img src={message.user.avatar_url} alt={message.user.name} />
+                        </div>
+                        <span>{message.user.name}</span>
+                     </div>
+                  </li>
+               );
+            })}
          </ul>
       </div>
    )
